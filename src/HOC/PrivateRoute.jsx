14fb58171661cce74c7b65cfb90167604b680fd9
hamed <52428565+hamednourzaei@users.noreply.github.com>
2025-05-15 09:26:41 +0330

    Update PrivateRoute.jsx

--- src/HOC/PrivateRoute.jsx
+++ src/HOC/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-
+ 
 const PrivateRoute = ({ children }) => {
   const { isAuth } = useSelector((state) => state.auth);
   console.log(isAuth);
